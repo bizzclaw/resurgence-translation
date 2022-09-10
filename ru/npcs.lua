2332ef2805c00887d28350d1674bbe6aefbcd526
Blueberryy <36592509+Blueberryy@users.noreply.github.com>
2022-09-10 22:47:09 +0500

    update ru

--- ru/npcs.lua
+++ ru/npcs.lua
@@ -57,21 +57,87 @@ lang["#NPC_MIRELURKS"] = "Болотников"
 -- Ghouls
 lang["#NPC_FERALGHOUL"] = "Дикого гуля"
 lang["#NPC_FERALGHOULS"] = "Диких гулей"
-lang["#NPC_FERALGHOUL_REAVER"] = "Гуля-потрошителя"
-lang["#NPC_FERALGHOUL_REAVERS"] = "Гулей-потрошителей"
+lang["#NPC_FERALGHOUL_REAVER"] = "Дикого гуля-потрошителя"
+lang["#NPC_FERALGHOUL_REAVERS"] = "Диких гулей-потрошителей"
+
+lang["#NPC_FERALRIFLEMAN"] = "Дикого стрелка"
+lang["#NPC_FERALMARKSMAN"] = "Дикого стрелка-снайпера"
+lang["#NPC_FERALSNIPER"] = "Дикого снайпера"
+lang["#NPC_FERALGUNNER"] = "Дикого пулемётчика"
+lang["#NPC_FERALCOMMANDER"] = "Безумного командира"
+lang["#NPC_FERALGRENADIER"] = "Безумного подрывника"
+lang["#NPC_FERALFLAMER"] = "Дикого огнемётчика"
 -- Bandits
 lang["#NPC_BANDIT"] = "Рейдера"
 lang["#NPC_BANDITS"] = "Рейдеров"
+--80s tribe
 lang["#NPC_80SBANDIT"] = "Рейдера племени 80-х"
 lang["#NPC_80SBANDITS"] = "Рейдеров племени 80-х"
-lang["#NPC_ICEPICKBANDIT"] = "Ледоруба"
-lang["#NPC_ICEPICKBANDITS"] = "Ледорубов"
-lang["#NPC_ICEPICKBANDITSNIPERS"] = "Охотников за головами"
-lang["#NPC_ROCKBITERBANDIT"] = "Камнееда"
-lang["#NPC_ROCKBITERBANDITS"] = "Камнеедов"
+lang["#NPC_80SBANDIT_RANK1"] = "Панка племени 80-х"
+lang["#NPC_80SBANDIT_RANK2"] = "Рейдера племени 80-х"
+lang["#NPC_80SBANDIT_RANK2_OVERWATCH"] = "Дальнобойщика племени 80-х" -- A "Long Hauler" is an individual or vehicle that is involved in lengthy, long distance cargo trips
+lang["#NPC_80SBANDIT_RANK3"] = "Разбойника племени 80-х" -- a "Highwayman" is a name for a thief who opperates near commonly traveled roads
+lang["#NPC_80SBANDIT_RANK4"] = "Дорожного агента племени 80-х" -- another name for a "highwayman"
+lang["#NPC_80SBANDIT_RANK5"] = "Тягача племени 80-х" -- a "Big Rig" is a nickname for a Semitruck, the kind used to haul large cargo trailers
+-- Ice Picks
+lang["#NPC_ICEPICKBANDIT"] = "Рейдера Ледоруба" -- "Ice Pick", as in the tool
+lang["#NPC_ICEPICKBANDITS"] = "Рейдеров Ледорубов"
+lang["#NPC_ICEPICKBANDITSNIPERS"] = "Рейдеров-охотников за головами Ледорубов" --"Savage", as in a derogatory name for a primative
+lang["#NPC_ICEPICKBANDIT_RANK2_OVERWATCH"] = "Рейдера Ледорубов"
+lang["#NPC_ICEPICKBANDIT_RANK3"] = "Ловца людей Ледорубов" --"Man-Trapper", a name for someone who specializes in hunting and trapping people 
+lang["#NPC_ICEPICKBANDIT_RANK4"] = "Пожирателя плоти Ледорубов"
+lang["#NPC_ICEPICKBANDIT_RANK5"] = "Мизантропа Ледорубов"
+--Rock-Biters
+lang["#NPC_ROCKBITERBANDIT"] = "Рейдера Камнеедов"
+lang["#NPC_ROCKBITERBANDITS"] = "Рейдеров Камнеедов"
+lang["#NPC_ROCKBITERBANDIT_RANK1"] = "Старателя Камнеедов"
+lang["#NPC_ROCKBITERBANDIT_RANK2"] = "Шахтёра Камнеедов"
+lang["#NPC_ROCKBITERBANDIT_RANK2_OVERWATCH"] = "Землемера Камнеедов"
+lang["#NPC_ROCKBITERBANDIT_RANK3"] = "Бульдозера Камнеедов"
+lang["#NPC_ROCKBITERBANDIT_RANK4"] = "Бригадира Камнеедов"
+lang["#NPC_ROCKBITERBANDIT_RANK5"] = "Разрушителя жил Камнеедов"
+--Smugglers
+lang["#NPC_SMUGGLERBANDIT"] = "Контрабандиста"
+lang["#NPC_SMUGGLERRBANDITS"] = "Контрабандистов"
+lang["#NPC_SMUGGLERBANDIT_RANK1"] = "Бегуна контрабандистов"
+lang["#NPC_SMUGGLERBANDIT_RANK2"] = "Торговца контрабандистов"
+lang["#NPC_SMUGGLERBANDIT_RANK2_OVERWATCH"] = "Разведчика контрабандистов"
+lang["#NPC_SMUGGLERBANDIT_RANK3"] = "Наркодельца контрабандистов"
+lang["#NPC_SMUGGLERBANDIT_RANK4"] = "Лунного пророка контрабандистов" --"Moon-Curser", a name for someone who smuggles contraband during the dark of night
+lang["#NPC_SMUGGLERBANDIT_RANK5"] = "Торговца смертью контрабандистов" --"Death Merchant", a derogatory name for an Arms/Weapons dealer
+--Shadowed
+lang["#NPC_SHADOWEDBANDIT"] = "Рейдера теней"
+lang["#NPC_SHADOWEDBANDITS"] = "Рейдеров теней"
+lang["#NPC_SHADOWEDBANDIT_RANK1"] = "Халявщика теней"
+lang["#NPC_SHADOWEDBANDIT_RANK2"] = "Похитителя теней"
+lang["#NPC_SHADOWEDBANDIT_RANK2_OVERWATCH"] = "Наводчика теней"
+lang["#NPC_SHADOWEDBANDIT_RANK3"] = "Мародёра теней"
+lang["#NPC_SHADOWEDBANDIT_RANK4"] = "Взломщика домов теней"
+
+--Mercenarys
+lang["#NPC_MERC"] = "Наёмника"
+lang["#NPC_MERCS"] = "Наёмников"
+lang["#NPC_MERCLEADER"] = "Лидера наёмников"
+lang["#NPC_MERCSNIPER"] = "Снайпера наёмников"
+
+--Cipher Agents
+lang["#NPC_CIPHERAGENT"] = "Агент Шыфра"
+lang["#NPC_CIPHERAGENTS"] = "Агентов Шыфра"
+lang["#NPC_CIPHERAGENT_RANK1"] = "Интерна Шыфра"
+lang["#NPC_CIPHERAGENT_RANK2"] = "Подрядчика Шыфра"
+lang["#NPC_CIPHERAGENT_RANK3"] = "Партнёра Шыфра"
+lang["#NPC_CIPHERAGENT_RANK4"] = "Руководителя Шыфра"
+lang["#NPC_CIPHERAGENT_RANK5"] = "Директора Шыфра"
+lang["#NPC_CIPHERAGENT_SNIPER"] = "Наблюдателя"
+
 -- Super Mutants
 lang["#NPC_SUPERMUTANT"] = "Супермутанта"
 lang["#NPC_SUPERMUTANTS"] = "Супермутантов"
+--Super Mutant "Rejected" clan name
+lang["#NPC_REJECTEDSM_RANK1"] = "Бугая отвергнутых"
+lang["#NPC_REJECTEDSM_RANK2"] = "Бестию отвергнутых"
+lang["#NPC_REJECTEDSM_RANK3"] = "Мастера отвергнутых"
+lang["#NPC_REJECTEDSM_RANK4"] = "Босса отвергнутых"
 
 -- ██████╗  ██████╗ ██████╗  ██████╗ ████████╗██╗ ██████╗
 -- ██╔══██╗██╔═══██╗██╔══██╗██╔═══██╗╚══██╔══╝██║██╔════╝
