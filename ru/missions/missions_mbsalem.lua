be9b9962320b07d62fc0b808983e42bd0a98d4e0
Blueberryy <36592509+Blueberryy@users.noreply.github.com>
2024-10-08 21:06:55 +0500

    (ru) update translation for latest commit

---
+++ ru/missions/missions_mbsalem.lua
@@ -0,0 +1,33 @@
+local lang = RSRG.Languages["ru"]
+
+-- SALEM MISSION BOARD
+
+lang["#MISSION_BOARD_SALEM"] = "Доска заданий Сейлема"
+lang["#MISSION_BOARD_SALEM_SUBTITLE"] = "Задания и работы"
+lang["#MISSION_BOARD_SALEM_GREET"] = "Эта доска содержит несколько документов с заданиями от людей, которые хотят нанять других людей в этой области для их выполнения. Вы можете принять любую работу, которая соответствует вашим требованиям.\n\n\nСписок доступных заданий приведён ниже."
+
+-- MISSIONS
+
+lang["#MISSION_SALEM_SHADOWEDBOUNTY"] = "Награда за Теней"
+lang["#MISSION_SALEM_SHADOWEDBOUNTY_DESC"] = "Караван объявил охоту на бандитов из Тени. Убей 10 и принеси доказательства, чтобы получить награду."
+lang["#MISSION_SALEM_SHADOWEDBOUNTY_HINT"] = "Убейте бандитов Теней в Сейлеме."
+lang["#MISSION_SALEM_SHADOWEDBOUNTY_COMPLETE"] = "Вы выполнили задание. Поговорите с Джемсом, чтобы получить награду."
+
+lang["#MISSION_SALEM_POLICEBOUNTY"] = "Награда за полицейских"
+lang["#MISSION_SALEM_POLICEBOUNTY_DESC"] = "Караван объявил охоту на недавно восстановленных протектронов-полицейских. Уничтожь 10 и верни их чипы наведения на цель, чтобы получить награду."
+lang["#MISSION_SALEM_POLICEBOUNTY_HINT"] = "Уничтожьте протектронов-полицейских в Сейлеме."
+lang["#MISSION_SALEM_POLICEBOUNTY_COMPLETE"] = "Вы выполнили задание. Поговорите с Джемсом, чтобы получить награду."
+
+lang["#MISSION_SALEM_SUPERMUTANTBOUNTY"] = "Награда за супермутантов"
+lang["#MISSION_SALEM_SUPERMUTANTBOUNTY_DESC"] = "Караван объявил охоту на супермутантов в Сейлеме, они похищают беженцев, пытающихся добраться до безопасных мест. Уничтожь 15 из них."
+lang["#MISSION_SALEM_SUPERMUTANTBOUNTY_HINT"] = "Убейте супермутантов в Сейлеме."
+lang["#MISSION_SALEM_SUPERMUTANTBOUNTY_COMPLETE"] = "Вы выполнили задание. Поговорите с Джемсом, чтобы получить награду."
+
+lang["#MISSION_SALEM_AMMOPICKUP"] = "Груз боеприпасов"
+lang["#MISSION_SALEM_AMMOPICKUP_DESC"] = "Караван направил запрос о похищенном грузе, подробности таковы. Груз боеприпасов из Эшлэнда был перехвачен Тенями и предназначался для Маверика и его людей. Они пытаются вымогать у нас крышки, но мы не собираемся уступать. Верните груз из одного из их лагерей в Сейлеме, и вам хорошо заплатят. -Маверик"
+lang["#MISSION_SALEM_AMMOPICKUP_HINT"] = "Верните блестящий ящик боеприпасов из лагеря Теней в Сейлеме. Он может находиться в любом из них, так что приступайте к поискам."
+lang["#MISSION_SALEM_AMMOPICKUP_COMPLETE"] = "Вы выполнили задание. Поговорите с Джемсом, чтобы получить награду."
+lang["#MISSION_SALEM_AMMOPICKUP_ITEMNAME"] = "Украденный ящик боеприпасов"
+
+
+RELOADSCHEMA = true
